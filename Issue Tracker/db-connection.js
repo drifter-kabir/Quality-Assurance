43f9683f54e7594573768399748ca360ef82a445
Milton Kabir <31060254+drifter-kabir@users.noreply.github.com>
2021-05-28 11:17:25 +0600

    Update db-connection.js

--- Issue Tracker/db-connection.js
+++ Issue Tracker/db-connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-let uri = 'mongodb+srv://drifter_kabir:9vnww7NzNudbuUS@cluster0.9r2iq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+let uri = '';//Use your own
 const db = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = db;
